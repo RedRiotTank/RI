2d076a6a630f61db1925f47c3ed022efea9a91f2
Alberto Plaza <albertoplazamontesdm@gmail.com>
2023-09-22 14:26:12 +0200

    add: startpoint

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,23 @@
 package org.example;
+import org.apache.tika.Tika;
+import org.apache.tika.exception.TikaException;
+
+import java.io.File;
+import java.io.IOException;
 
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!!!");
+    public static void main(String[] args) throws IOException, TikaException {
+        Tika tika = new Tika();
+
+        for(String file : args){
+            File f = new File(file);
+            String type = tika.detect(f);
+            System.out.println(file + " : " + type);
+
+            String text = tika.parseToString(f);
+            System.out.println(text);
+        }
+
+
     }
 }
